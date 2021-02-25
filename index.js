88e0e07f8576ccdd2ec1f2a2781a570554e61a14
Leah Miller <leahmiller1969@gmail.com>
2021-02-25 12:22:18 -0800

    notes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-function calculator(num1, num2, operator){
+function calculator(num1, num2, operator){  // operater is considered a higher function. It takes a function as an parameter
     document.getElementById('calc').innerHTML = operator(num1, num2)
     
 }
